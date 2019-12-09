e812b853882a07712b8090598d73704b24fa3ab0
Bruno Morgado <morgado.fis@gmail.com>
2019-12-09 16:05:15 +0100

    Adding some basic functions to Star class

--- sora/star.py
+++ sora/star.py
@@ -29,17 +29,91 @@ class Star():
     Docstring
     Define a star
     '''
-    def __init__(self, **kwargs):
-        # Run initial parameters
-        #if 'coord' in kwargs:
-        #    self.star = test_attr(kwargs['coord'], SkyCoord, 'coord')
-        #elif 'code' in kwargs:
-        #    self.star = test_attr(kwargs['code'], str, 'code')
-        #else:
-        #    raise KeyError('Input values must have either the coordinate of the star or the Gaia code for online search')
-        #if type(self.star) != SkyCoord:
-        #    pos = self.search_gaia2()
+    def __init__(self,**kwargs):
+        self.ra = None
+        self.dec = None
+        self.magG = None
+        self.magB = None
+        self.magV = None
+        self.magK = None
+        self.diameter = None
         return
+    
+    def set_magnitudes(self,magG,magB,magV,magK):
+        '''
+        Set the magnitudes of a star in the G, B, V and K band.
+        usually this values can be found in the GDR2 and in the NOMAD catalogue.
+        Inputs:
+        radius = float, in mas
+        '''
+        self.magG = magG
+        self.magB = magB
+        self.magV = magV
+        self.magK = magK
+        return
+
+    def set_diameter(self,star_diameter):
+        '''
+        Set the diameter of a star in mas.
+        Inputs:
+        star_diameter = float, in mas
+        '''
+        self.diameter = star_diameter
+        return
+
+
+    def calc_diameter(self,obs_filter='V',star_type='sg'):
+        '''
+        Determine the diameter of a star in mas using equations from van Belle (1999) 
+        -- Publi. Astron. Soc. Pacific 111, 1515-1523:
+        Inputs:
+        obs_filter = str; should be Visual (V) or Blue (B)
+        star_type = str; should be Super-giant (gs), Main-sequence (ms) or Variable (vs)
+        '''
+        if (self.magV == None):
+            print('First you need to set the magnitudes B, V, K')
+            return
+        if (obs_filter not in ['V','B']):
+            print('obs_filter should be Visual (V) or Blue (B)')
+            return
+        if (star_type not in ['sg','ms','vs']):
+            print('star_type should be Super-giant (gs), Main-sequence (ms) or Variable (vs)')
+            return
+        if (star_type == 'sg'):
+            if (obs_filter == 'B'):
+                a1, a2 = 0.648, 0.220
+                self.diameter = (10**(a1 + a2*(self.magB - self.magK) - 0.2*self.magB))
+            if (obs_filter == 'V'):
+                a1, a2 = 0.669, 0.223
+                self.diameter = (10**(a1 + a2*(self.magV - self.magK) - 0.2*self.magV))
+        if (star_type == 'ms'):
+            if (obs_filter == 'B'):
+                a1, a2 = 0.500, 0.290
+                self.diameter = (10**(a1 + a2*(self.magB - self.magK) - 0.2*self.magB))
+            if (obs_filter == 'V'):
+                a1, a2 = 0.500, 0.264
+                self.diameter = (10**(a1 + a2*(self.magV - self.magK) - 0.2*self.magV))
+        if (star_type == 'vs'):
+            if (obs_filter == 'B'):
+                a1, a2 = 0.789, 0.218
+                self.diameter = (10**(a1 + a2*(self.magB - self.magK) - 0.2*self.magB))
+            if (obs_filter == 'V'):
+                a1, a2 = 0.840, 0.211
+                self.diameter = (10**(a1 + a2*(self.magV - self.magK) - 0.2*self.magV))
+        return
+
+
+    #def __init__(self, **kwargs):
+    #    # Run initial parameters
+    #    #if 'coord' in kwargs:
+    #    #    self.star = test_attr(kwargs['coord'], SkyCoord, 'coord')
+    #    #elif 'code' in kwargs:
+    #    #    self.star = test_attr(kwargs['code'], str, 'code')
+    #    #else:
+    #    #    raise KeyError('Input values must have either the coordinate of the star or the Gaia code for online search')
+    #    #if type(self.star) != SkyCoord:
+    #    #    pos = self.search_gaia2()
+    #    return
     
     def apparent_radius(self, distance):
         # calculate the apparent radius of the star at given distance
