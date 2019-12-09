664a1cda520e5aabf41a96c8a95d6a9bc8931002
Altair <altairgomesjr@gmail.com>
2019-12-09 17:55:45 +0000

    small modifications to docstring

--- sora/config.py
+++ sora/config.py
@@ -12,8 +12,8 @@ class colors():
 ### useful functions
 def test_attr(attr, typ, name):
     """
-    This function tests if the attr belongs to the type typ,
-    if not it gives an error message.
+    This function tests if the attribute "attr" belongs to the type "typ",
+    if not it gives an error message informing the name of the variable
     """
     try:
         return typ(attr)
