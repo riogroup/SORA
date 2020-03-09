ab00cc7645181aafd021d3809782be7a4e1e0270
Rodrigo Boufleur <rcboufleur@gmail.com>
2020-03-09 15:44:28 -0300

    added detction functionality to lightcurve class

--- sora/lightcurve.py
+++ sora/lightcurve.py
@@ -8,11 +8,12 @@ class LightCurve():
     Docstring
     Define a Light Curve
     '''
-    def __init__(self,time,flux,**kwargs):
+    def __init__(self, time, flux, dflux=None, **kwargs):
         if len(flux) != len(time):
             print('The time and the flux should have the same length')
             return
         self.flux = flux
+        self.dflux = dflux
         self.time = time
         self.exptime = None
         self.model = np.ones(len(self.time))
@@ -121,4 +122,157 @@ class LightCurve():
         else:
             print('There is no lightcurve to plot')
         return
+    
+        def DetectOcc(self, maximum_duration=None, dur_step=None, snr_limit=None, \
+                  n_detections=None):
+        """
+        Detect automatically the occultation event in the light curve
+        
+        Parameters
+        ----------
+        maximum_duration: float, optional
+            Maximum duration of the occultation event (default is 1/4th of the
+            light curve's time span).
+        dur_step: float, optionl
+            Step size to sweep occultation duration event (default value is 1/2
+            of sampling).
+        snr_limit: float, optional
+            Minimum occultation SNR.
+        n_detections: int, optional
+            N best detections regardless from SNR. n_detections is superseded
+            by snr_limit.
+            
+        Returns
+        -------
+        OrderedDict
+            An ordered dictionary of :attr:`name`::attr:`value` pairs for each
+            Parameter.
+            
+        Examples
+        --------
+        >>> params = lc.DetectOcc()
+        >>> params
+        {'occultation_duration': 0.0004645648878067732,
+         'central_time': 2457852.5916293273,
+         'imersion_time': 2457852.5913970447,
+         'emersion_time': 2457852.59186161,
+         'time_err': 5.799811333417892e-07,
+         'depth': 0.8663887801707082,
+         'depth_err': 0.10972550419008305,
+         'baseline': 0.9110181732552853,
+         'baseline_err': 0.1904360360568157,
+         'snr': 91.21719495827487,
+         'occ_mask': array([False, False, False, ..., False, False, False])}
+        """
+
+        # duration of the light curve
+        time_span = self.time[-1]-self.time[0]
+        if maximum_duration and (maximum_duration > time_span):
+            raise ValueError('Occultation duration (maximum_duration={0}) ' \
+                             'exceeds the time series lenght ({1:0.5f}).' \
+                             .format(maximum_duration, time_span))
+        if not maximum_duration:
+            maximum_duration = time_span*0.25
+        if not dur_step:
+            dur_step = np.median(self.time[1:-1]-self.time[0:-2])/2      
+        duration_grid = np.arange(dur_step, maximum_duration, dur_step)
+        # initial occultation mask (all data points)
+        mask = np.ones(len(self.time), dtype=bool)
+
+        if snr_limit:
+            # minimum SNR accepted in a detection for multiple search
+            snr_value = snr_limit+1
+            occ0 = self._runBLS(time_span, duration_grid)
+            mask *= ~occ0['occ_mask']
+            while (snr_value > snr_limit):
+                occ1 = self._runBLS(time_span, duration_grid, mask=mask)
+                if occ1['snr'] > snr_limit:
+                    snr_value = occ1['snr']
+                    mask *= ~occ1['occ_mask']                    
+                    occ0 = self._summarizeBLS(occ0,occ1)
+                else:
+                    snr_value = snr_limit
+            return occ0
+        elif n_detections:
+            # search the n best fits
+            occ0 = self._runBLS(time_span, duration_grid)
+            mask *= ~occ0['occ_mask']    
+            for i in range(n_detections-1):
+                occ1 = self._runBLS(time_span, duration_grid, mask=mask)
+                snr_value = occ1['snr']
+                mask *= ~occ1['occ_mask']                    
+                occ0 = self._summarizeBLS(occ0,occ1)
+            return occ0
+        else:
+            # search only the first best fit
+            return self._runBLS(time_span, duration_grid)
+        
+        
+    def _runBLS(self, per_grid, dur_grid, mask=None):
+        """
+        Private function to find the best box fit suitable to the data
+        """
+
+        # object with no occultation mask
+        mskmodel = BoxLeastSquares(self.time, self.flux, dy=self.dflux)
+        # if there is no dflux array, reset it to None in case of 
+        # using a mask
+        if self.dflux is None:
+            dfluxmask = None
+        else:
+            dfluxmask = self.dflux[mask]
+            
+        # object with occultation mask
+        if np.sum(mask):
+            model = BoxLeastSquares(self.time[mask], self.flux[mask], \
+                                    dy=dfluxmask)
+        else:
+            model = mskmodel   
+        
+        r = model.power(per_grid, dur_grid, objective='snr', method='fast')
+        # statistics of the BLS fit
+        stats = model.compute_stats(r.period, r.duration, r.transit_time)
+        # occultation mask of the event with respect to all data
+        occ_mask = mskmodel.transit_mask(self.time, r.period, r.duration, \
+                                         r.transit_time)
+        # parameters computation for clarity purposes
+        occultation_duration = r.duration[0]
+        central_time = stats['transit_times'][0]
+        imersion_time = stats['transit_times'][0] - r.duration[0]/2
+        emersion_time = stats['transit_times'][0] + r.duration[0]/2
+        time_err = dur_grid[1] - dur_grid[0]
+        depth = np.mean(self.flux[~occ_mask])-np.mean(self.flux[occ_mask])
+        depth_err = np.std(self.flux[occ_mask])
+        baseline = np.mean(self.flux[~occ_mask])
+        baseline_err = np.std(self.flux[~occ_mask])
+        snr = (depth/baseline_err)*np.sqrt(np.sum(occ_mask))
+
+        return {'occultation_duration' : occultation_duration, 
+                     'central_time' : central_time, 
+                     'imersion_time' : imersion_time, 
+                     'emersion_time' : emersion_time,
+                     'time_err' : time_err,
+                     'depth' : depth, 'depth_err' : depth_err,
+                     'baseline' : baseline, 'baseline_err' : baseline_err,
+                     'snr' : snr,'occ_mask' : occ_mask}
+    
+    def _summarizeBLS(self, dict1, dict2):
+        ''' Private function to merge dictionaries returned by BLS and 
+            keep values of common keys in list.
+        '''
+        dict3 = {}
+        for key, value in dict1.items():
+            if key == 'occ_mask':
+                sz = int(np.size(dict1[key])/np.size(dict2[key]))
+                if sz > 1:
+                    l = [None]*(sz+1)
+                    for i in range(sz):
+                        l[i] = dict1[key][i]
+                    l[i+1] = dict2[key]
+                    dict3[key] = l
+                else:
+                    dict3[key] = [dict1[key],dict2[key]]
+            else:
+                dict3[key] = np.append(dict1[key],dict2[key])
+        return dict3
 
