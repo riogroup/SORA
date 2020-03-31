74216c65c7860d0aca77adc057621317599bd179
Altair <altairgomesjr@gmail.com>
2020-03-31 21:04:02 +0000

    Allow draw_ellipse to run many ellipsis and Added get_values funcion to ChiSquare

--- sora/extra.py
+++ sora/extra.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import astropy.units as u
 
-def draw_ellipse(radius, oblateness=0.0, center_x=0.0, center_y=0.0, pos_angle=0.0, *args, **kwargs):
+def draw_ellipse(equatorial_radius, oblateness=0.0, center_f=0.0, center_g=0.0, position_angle=0.0, *args, **kwargs):
     """ Plots an ellipse given input parameters
 
     Parameters:
@@ -13,18 +13,39 @@ def draw_ellipse(radius, oblateness=0.0, center_x=0.0, center_y=0.0, pos_angle=0
         pos_angle (float, int): Pole position angle. Default=0.0
         **kwargs: all other parameters will be parsed directly to matplotlib
     """
+    equatorial_radius = np.array(equatorial_radius, ndmin=1)
+    oblateness = np.array(oblateness, ndmin=1)
+    center_f = np.array(center_f, ndmin=1)
+    center_g = np.array(center_g, ndmin=1)
+    position_angle = np.array(position_angle, ndmin=1)
+    
     theta= np.linspace(-np.pi, np.pi, 1800)
-    circle_x =  radius*np.cos(theta)
-    circle_y =  radius*(1.0-oblateness)*np.sin(theta)
-    if 'color' not in kwargs:
-        kwargs['color'] = 'black'
-    if 'lw' not in kwargs:
-        kwargs['lw'] = 2
-    plt.plot(circle_x*np.cos(pos_angle*u.deg) + circle_y*np.sin(pos_angle*u.deg) + center_x,
-             -circle_x*np.sin(pos_angle*u.deg) + circle_y*np.cos(pos_angle*u.deg) + center_y,
-             *args, **kwargs)
+    size_vals, size_theta = np.indices((len(equatorial_radius), len(theta)))
     
     
+    if len(equatorial_radius) == 1:
+        if 'color' not in kwargs:
+            kwargs['color'] = 'black'
+        if 'lw' not in kwargs:
+            kwargs['lw'] = 2
+    else:
+        if 'color' not in kwargs:
+            kwargs['color'] = 'gray'
+        if 'lw' not in kwargs:
+            kwargs['lw'] = 0.1
+        if 'alpha' not in kwargs:
+            kwargs['alpha'] = 0.1
+        if 'zorder' not in kwargs:
+            kwargs['zorder'] = 0.5
+    for i in np.arange(len(equatorial_radius)):
+        circle_x =  equatorial_radius[i]*np.cos(theta)
+        circle_y =  equatorial_radius[i]*(1.0-oblateness[i])*np.sin(theta)
+        plt.plot(circle_x*np.cos(position_angle[i]*u.deg) + circle_y*np.sin(position_angle[i]*u.deg) + center_f[i],
+                 -circle_x*np.sin(position_angle[i]*u.deg) + circle_y*np.cos(position_angle[i]*u.deg) + center_g[i],
+                 *args, **kwargs)
+    plt.axis('equal')
+
+
 class ChiSquare():
     """ ChiSquare stores the arrays for all inputs and given chi-square.
 
@@ -42,16 +63,15 @@ class ChiSquare():
 
     """
     def __init__(self,chi2,npts,**kwargs):
-        names = [('chi2', 'f8')]
-        data = chi2
+        self.__names = ['chi2']
+        self.data = {'chi2': chi2}
         data_size = len(chi2)
         self.npts = npts
         for item in kwargs.keys():
             if len(kwargs[item]) != data_size:
                 raise ValueError('{} size must have the same size as given chi2')
-            names.append((item, 'f8'))
-            data = np.vstack((data, kwargs[item]))
-        self.data = np.array([tuple(i) for i in data.T], dtype=names)
+            self.__names.append(item)
+            self.data[item] = kwargs[item]
         
     def get_nsigma(self,sigma=1, key=None):
         """ Determines the interval of the chi-square within the n-th sigma
@@ -72,14 +92,14 @@ class ChiSquare():
         output = {'chi2_min': self.data['chi2'].min()}
         output['sigma'] = sigma
         output['n_points'] = len(values)
-        for name in self.data.dtype.names[1:]:
+        for name in self.__names[1:]:
             vmax = self.data[name][values].max()
             vmin = self.data[name][values].min()
             output[name] = [(vmax+vmin)/2.0, (vmax-vmin)/2.0]
         if key is not None:
-            if key not in self.data.dtype.names[1:]:
+            if key not in self.__names[1:]:
                 raise ValueError('{} is not one of the available keys. Please choose one of {}'
-                                 .format(key, self.data.dtype.names[1:]))
+                                 .format(key, self.__names[1:]))
             return output[key]
         return output
     
@@ -92,13 +112,14 @@ class ChiSquare():
         """
         sigma_1 = self.get_nsigma(sigma=1)
         sigma_3 = self.get_nsigma(sigma=3)
-        if key is not None and (key not in self.data.dtype.names[1:]):
+        if key is not None and (key not in self.__names[1:]):
             raise ValueError('{} is not one of the available keys. Please choose one of {}'
-                                 .format(key, self.data.dtype.names[1:]))
-        for name in self.data.dtype.names[1:]:
+                                 .format(key, self.data.keys()[1:]))
+        k = np.where(self.data['chi2'] < sigma_1['chi2_min']+11)
+        for name in self.__names[1:]:
             if (key is not None) and (key != name):
                 continue
-            plt.plot(self.data[name], self.data['chi2'], 'k.')
+            plt.plot(self.data[name][k], self.data['chi2'][k], 'k.')
             plt.ylim(sigma_1['chi2_min']-1, sigma_1['chi2_min']+10)
             plt.xlim(sigma_3[name][0]-3*sigma_3[name][1], sigma_3[name][0]+3*sigma_3[name][1])
             plt.axhline(sigma_1['chi2_min']+1,linestyle='--',color='red')
@@ -115,11 +136,22 @@ class ChiSquare():
         Parameters:
             namefile (str): Filename to save the data
         """
-        np.savetxt(namefile, self.data, fmt='%10.5f')
+        data = np.vstack(([self.data[i] for i in self.__names]))
+        np.savetxt(namefile, data.T, fmt='%11.5f')
         f = open(namefile+'.label', 'w')
-        for i, name in enumerate(self.data.dtype.names):
+        for i, name in enumerate(self.__names):
             f.write('Column {}: {}\n'.format(i+1,name))
         f.close()
+
+    def get_values(self, sigma=0.0):
+        values = {}
+        if sigma == 0.0:
+            k = np.argsort(self.data['chi2'])[0]
+        else:
+            k = np.where(self.data['chi2'] < self.data['chi2'].min() + sigma**2)[0]
+        for name in self.__names[1:]:
+            values[name] = self.data[name][k]
+        return values
                 
     def __str__(self):
         """ String representation of the ChiSquare Object
@@ -129,7 +161,7 @@ class ChiSquare():
         output = 'Minimum chi-square: {:.3f}\n'.format(sigma_1['chi2_min'])
         output += 'Number of fitted points: {}\n'.format(self.npts)
         output += 'Minimum chi-square per degree of freedom: {:.3f}\n'.format(sigma_1['chi2_min']/self.npts)
-        for name in self.data.dtype.names[1:]:
+        for name in self.__names[1:]:
             output += '\n{}:\n'.format(name)
             output += '    1-sigma: {:.3f} +/- {:.3f}\n'.format(*sigma_1[name])
             output += '    3-sigma: {:.3f} +/- {:.3f}\n'.format(*sigma_3[name])
