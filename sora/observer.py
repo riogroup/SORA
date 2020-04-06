69247d2099417a440b6d0996c3a93e07519dace3
Altair <altairgomesjr@gmail.com>
2020-04-06 16:42:18 +0000

    Added function to univocally define Observer and allow redefinition of coordinates.

--- sora/observer.py
+++ sora/observer.py
@@ -26,6 +26,7 @@ class Observer():
     Docstring
     Define the observer object
     '''
+    __names = []
     def __init__(self, *args, **kwargs):
         # Run initial parameters
         if 'name' not in kwargs:
@@ -41,14 +42,14 @@ class Observer():
                     pass
             self.code = kwargs['code']
             try:
-                self.name, self.site = search_code_mpc()[self.code]
+                self.__name, self.site = search_code_mpc()[self.code]
             except:
                 raise ValueError('code {} could not be located in MPC database'.format(self.code))
         elif len(args) == 2 or all(i in kwargs for i in ['name', 'site']):
             if len(args) == 2:
                 kwargs['name'] = args[0]
                 kwargs['site'] = args[1]
-            self.name = kwargs['name']
+            self.__name = kwargs['name']
             self.site = test_attr(kwargs['site'], EarthLocation, 'site')
         elif len(args) == 4 or all(i in kwargs for i in ['name', 'lon', 'lat', 'height']):
             if len(args) == 4:
@@ -56,10 +57,13 @@ class Observer():
                 kwargs['lon'] = args[1]
                 kwargs['lat'] = args[2]
                 kwargs['height'] = args[3]
-            self.name = kwargs['name']
+            self.__name = kwargs['name']
             self.site = EarthLocation(kwargs['lon'], kwargs['lat'], kwargs['height'])
         else:
             raise ValueError('Input parameters could not be determined')
+        if self.__name in self.__names:
+            raise ValueError('name {} already defined for another Observer object. Please choose a different one.'.format(self.__name))
+        self.__names.append(self.__name)
         
     def get_ksi_eta(self, time, star):
         """ Calculates relative position to star in the orthographic projection.
@@ -98,6 +102,43 @@ class Observer():
         else:
             raise ValueError('mode must be "local" or "greenwich"')
 
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def lon(self):
+        return self.site.lon
+
+    @lon.setter
+    def lon(self, lon):
+        lat = self.site.lat
+        height = self.site.height
+        site = EarthLocation(lon, lat, height)
+        self.site = site
+
+    @property
+    def lat(self):
+        return self.site.lat
+
+    @lat.setter
+    def lat(self, lat):
+        lon = self.site.lon
+        height = self.site.height
+        site = EarthLocation(lon, lat, height)
+        self.site = site
+
+    @property
+    def height(self):
+        return self.site.height
+
+    @height.setter
+    def height(self, height):
+        lon = self.site.lon
+        lat = self.site.lat
+        site = EarthLocation(lon, lat, height)
+        self.site = site
+
     def __str__(self):
         """String representation of the Observer class
         """
@@ -105,3 +146,9 @@ class Observer():
         out += 'Geodetic coordinates: Lon: {}, Lat: {}, height: {:.3f}'.format(
             self.site.lon.__str__(), self.site.lat.__str__(), self.site.height.to(u.km))
         return out
+
+    def __del__(self):
+        try:
+            self.__names.remove(self.__name)
+        except:
+            pass
