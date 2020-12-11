0ef4cf9bd9c63c02780465a04af3f476395e0860
Bruno Morgado <morgado.fis@gmail.com>
2020-12-11 09:04:34 -0300

    Version updated to v0.1.2 in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 name='sora',
-version='0.1.1',
+version='0.1.2',
 packages=find_packages(),
 license=open('LICENSE', encoding="utf-8").read(),
 long_description=open('README.md', encoding="utf-8").read(),
