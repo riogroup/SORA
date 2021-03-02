3d0083b4c1bb41a7793d7aa4cfb3bd583444a4ac
Altair Gomes <altairgomesjr@gmail.com>
2021-03-02 13:34:35 -0300

    Reorganizing the lightcurve module into separated files

---
+++ sora/lightcurve/occdectect.py
@@ -0,0 +1,224 @@
+import warnings
+import numpy as np
+
+__all__ = ['occ_detect']
+
+
+def occ_detect(flux, dflux, time, cycle, maximum_duration=None, dur_step=None, snr_limit=None,
+               n_detections=None, plot=False):
+    """ Detects automatically the occultation event in the light curve
+        (detects a 'square well transit')
+
+    Parameters:
+    (All parameters are optional)
+        maximum_duration (float): Maximum duration of the occultation event. Default: light curve's time span
+        dur_step (float): Step size to sweep occultation duration event. Default=1/2 of sampling
+        snr_limit (float): Minimum occultation SNR. Default=none
+        n_detections (int): Number of detections regardless the SNR.
+            n_detections is superseded by snr_limit. Default=1
+        plot (boolean): True if output plots are desired.
+
+    Returns:
+        OrderedDict = An ordered dictionary of :attr:`name`::attr:`value` pairs for each Parameter.
+
+    Examples:
+        >>> lc = LightCurve(time=time, flux=flux, exptime=0.0, name='lc_example')
+        >>> params = lc.occ_detect()
+        >>> params
+        {'rank': 1,
+        'occultation_duration': 40.1384063065052,
+        'central_time': 7916.773870512843,
+        'immersion_time': 7896.7046673595905,
+        'emersion_time': 7936.843073666096,
+        'time_err': 0.05011036992073059,
+        'depth': 0.8663887801707082,
+        'depth_err': 0.10986223384336465,
+        'baseline': 0.9110181732552853,
+        'baseline_err': 0.19045768512595365,
+        'snr': 7.886138392251848,
+        'occ_mask': array([False, False, False, ..., False, False, False])}
+    """
+
+    # duration of the light curve
+    time_span = time[-1] - time[0]
+    if maximum_duration and (maximum_duration > time_span):
+        warnings.warn('Occultation duration (maximum_duration={0}) '
+                      'exceeds the time series lenght ({1:0.5f}).'
+                      ' maximum_duration reset to the time series lenght.'
+                      .format(maximum_duration, time_span))
+        maximum_duration = time_span
+    if not maximum_duration:
+        maximum_duration = time_span
+
+    if not dur_step:
+        dur_step = cycle / 2
+
+    if dur_step < cycle / 2:
+        warnings.warn('The given dur_step is oversampled by a factor '
+                      'of {0:0.1f} and has been reset to half a cycle '
+                      '({1:0.4f}).'.format((cycle / 2.) / dur_step, cycle / 2.))
+        dur_step = cycle / 2
+
+    duration_grid = np.arange(dur_step, maximum_duration, dur_step)
+    # initial occultation mask (all data points)
+    mask = np.ones(len(time), dtype=bool)
+    # inital detection rank
+    rank = 1
+
+    if snr_limit:
+        # minimum SNR accepted in a detection for multiple search
+        snr_value = snr_limit + 1
+        occ0 = run_bls(flux, dflux, time, time_span, duration_grid)
+        mask *= ~occ0['occ_mask']
+        while snr_value > snr_limit:
+            rank += 1
+            occ1 = run_bls(flux, dflux, time, time_span, duration_grid, mask=mask,
+                           rank=rank)
+            if occ1['snr'] > snr_limit:
+                snr_value = occ1['snr']
+                mask *= ~occ1['occ_mask']
+                occ0 = summarize_bls(occ0, occ1)
+            else:
+                snr_value = snr_limit
+
+        if plot:
+            plot_occ_detect(occ0, flux, time)
+        return occ0
+    elif n_detections:
+        # search the n best fits
+        occ0 = run_bls(flux, dflux, time, time_span, duration_grid)
+        mask *= ~occ0['occ_mask']
+        for i in range(n_detections - 1):
+            rank += 1
+            occ1 = run_bls(flux, dflux, time, time_span, duration_grid, mask=mask,
+                           rank=rank)
+            mask *= ~occ1['occ_mask']
+            occ0 = summarize_bls(occ0, occ1)
+
+        if plot:
+            plot_occ_detect(occ0, flux, time)
+        return occ0
+    else:
+        # search only the first best fit
+        occ0 = run_bls(flux, dflux, time, time_span, duration_grid)
+
+        if plot:
+            plot_occ_detect(occ0, flux, time)
+
+        return occ0
+
+
+def plot_occ_detect(occ, flux, time):
+    import matplotlib.pyplot as plt
+
+    n = np.size(occ['rank'])
+    if n > 1:
+        # case for multiple detections
+        plt.plot(time, flux, 'k.-')
+        mask = np.zeros(len(time), dtype=bool)
+        for i in range(n):
+            trues = np.sum(occ['occ_mask'][i])
+            plt.plot(time[occ['occ_mask'][i]], np.repeat(np.mean(flux[occ['occ_mask'][i]]), trues),
+                     '.', label='Rank: ' + str(i + 1))
+            mask += occ['occ_mask'][i]
+
+        falses = list(mask).count(False)
+        plt.plot(time[~mask], np.repeat(np.mean(flux[~mask]), falses), 'r.', label='Baseline')
+        plt.xlabel('Time [seconds]')
+        plt.ylabel('Relative Flux')
+        plt.legend()
+
+    else:
+        # case for single occultation
+        trues = list(occ['occ_mask']).count(True)
+        falses = list(occ['occ_mask']).count(False)
+        plt.plot(time, flux, 'k.-')
+        plt.plot(time[occ['occ_mask']], np.repeat(np.mean(flux[occ['occ_mask']]), trues),
+                 '.', label='Occultation')
+        plt.plot(time[~occ['occ_mask']], np.repeat(np.mean(flux[~occ['occ_mask']]), falses),
+                 'r.', label='Baseline')
+        plt.xlabel('Time [seconds]')
+        plt.ylabel('Relative Flux')
+        plt.legend()
+
+
+def run_bls(flux, dflux, time, per_grid, dur_grid, mask=None, rank=None):
+    """ Private function to find the best box fit suitable to the data
+    """
+    from astropy.timeseries import BoxLeastSquares
+
+    # object with no occultation mask
+    mskmodel = BoxLeastSquares(time, flux, dy=dflux)
+    # if there is no dflux array, reset it to None in case of
+    # using a mask
+    if dflux is None:
+        dfluxmask = None
+    else:
+        dfluxmask = dflux[mask]
+
+    # object with occultation mask
+    if np.sum(mask):
+        model = BoxLeastSquares(time[mask], flux[mask],
+                                dy=dfluxmask)
+    else:
+        model = mskmodel
+
+    r = model.power(per_grid, dur_grid, objective='snr', method='fast')
+    # statistics of the BLS fit
+    stats = model.compute_stats(r.period, r.duration, r.transit_time)
+    # occultation mask of the event with respect to all data
+    occ_mask = mskmodel.transit_mask(time, r.period, r.duration,
+                                     r.transit_time)
+    # parameters computation for clarity purposes
+    occultation_duration = r.duration[0]
+    central_time = stats['transit_times'][0]
+    immersion_time = stats['transit_times'][0] - r.duration[0] / 2
+    emersion_time = stats['transit_times'][0] + r.duration[0] / 2
+    time_err = np.median(time[1:-1] - time[0:-2]) / 2
+    depth = np.mean(flux[~occ_mask]) - np.mean(flux[occ_mask])
+    depth_err = np.std(flux[occ_mask], ddof=1)
+    baseline = np.mean(flux[~occ_mask])
+    baseline_err = np.std(flux[~occ_mask], ddof=1)
+    # If there is only one measurement during the occultation it will
+    # use the baseline_err to compute SNR, otherwise it will use depth_err
+    if np.sum(occ_mask) < 2:
+        snr = depth / baseline_err
+    else:
+        snr = depth / depth_err
+
+    # define rank
+    if rank:
+        rank = rank
+    else:
+        rank = 1
+
+    return {'rank': rank,
+            'occultation_duration': occultation_duration,
+            'central_time': central_time,
+            'immersion_time': immersion_time,
+            'emersion_time': emersion_time,
+            'time_err': time_err,
+            'depth': depth, 'depth_err': depth_err,
+            'baseline': baseline, 'baseline_err': baseline_err,
+            'snr': snr, 'occ_mask': occ_mask}
+
+
+def summarize_bls(dict1, dict2):
+    """ Function to merge dictionaries returned by BLS and
+        to keep values of common keys in list.
+    """
+    dict3 = {}
+    for key, value in dict1.items():
+        if key == 'occ_mask':
+            sz = int(np.size(dict1[key]) / np.size(dict2[key]))
+            if sz > 1:
+                k = [None] * (sz + 1)
+                for i in range(sz):
+                    k[i] = dict1[key][i]
+                k[i + 1] = dict2[key]
+                dict3[key] = k
+            else:
+                dict3[key] = [dict1[key], dict2[key]]
+        else:
+            dict3[key] = np.append(dict1[key], dict2[key])
+    return dict3
---
+++ sora/lightcurve/utils.py
@@ -0,0 +1,184 @@
+import os
+
+import numpy as np
+
+from sora.config.decorators import deprecated_alias
+
+__all__ = ['calc_fresnel', 'calc_magnitude_drop']
+
+
+@deprecated_alias(lambida='bandpass')  # remove this line for v1.0
+def calc_fresnel(distance, bandpass):
+    """ Calculates the Fresnel scale.
+        (Fresnel Scale = square root of half the multiplication of wavelength and object distance.)
+
+    Parameters:
+        distance  (int, float, array): distances, in km.
+        bandpass  (int, float, array): wavelength, in km.
+
+    Returns:
+        fresnel_scale  (float, array): Fresnel scale, in km
+    """
+    return np.sqrt(bandpass * distance / 2)
+
+
+def bar_fresnel(X, X01, X02, fresnel_scale, opacity):
+    """ Returns the modelled light curve considering fresnel difraction.
+
+    Parameters:
+        X (array): Array with time values converted in km using the event velocity.
+        X01 (int, float): Immersion time converted in km using the event velocity.
+        X02 (int, float): Emersion time converted in km using the event velocity.
+        fresnel_scale (int, float): Fresnel scale, in km.
+        opacity (int, float): Opacity. Opaque = 1.0, transparent = 0.0
+
+    Returns:
+        flux_fresnel (array): the light curve with fresnel diffraction
+    """
+    import scipy.special as scsp
+
+    # Converting from km to units of fresnel scale
+    x = X / fresnel_scale
+    x01 = X01 / fresnel_scale
+    x02 = X02 / fresnel_scale
+    # Fresnel difraction parameters
+    x1 = x - x01
+    x2 = x - x02
+    s1, c1 = scsp.fresnel(x1)
+    s2, c2 = scsp.fresnel(x2)
+    cc = c1 - c2
+    ss = s1 - s2
+    r_ampli = - (cc + ss) * (opacity / 2.)
+    i_ampli = (cc - ss) * (opacity / 2.)
+    # Determining the flux considering fresnel difraction
+    flux_fresnel = (1.0 + r_ampli) ** 2 + i_ampli ** 2
+    return flux_fresnel
+
+
+def fit_pol(x, y, deg):
+    """ Fits a n-degree polynom to the data.
+
+    Parameters:
+        x (array): x-values
+        y (array): y-values
+        deg (int): degree of the polynom
+
+    Returns:
+        param (array): Array with the fitted values
+        param_err (array): Array with the errors of the fitted values
+    """
+    from scipy.odr import odrpack as odr
+    from scipy.odr import models
+
+    func = models.polynomial(deg)
+    mydata = odr.Data(x, y)
+    myodr = odr.ODR(mydata, func, maxit=200)
+    myodr.set_job(fit_type=2)
+    fit = myodr.run()
+    param = fit.beta[::-1]
+    param_err = fit.sd_beta[::-1]
+    return param, param_err
+
+
+def calc_magnitude_drop(mag_star, mag_obj):
+    """ Determines the magnitude drop of the occultation
+
+    Parameters:
+        mag_star (int,float): Star magnitude.
+        mag_obj (int,float): Object apparent magnitude to the date.
+
+    Returns:
+        mag_drop (float): Magnitude drop for the given magnitudes
+        bottom_flux (float): Normalized bottom flux for the given magnitudes
+    """
+    contrast = 1 / (1 + (10 ** ((mag_star - mag_obj) * 0.4)))
+    mag_combined = mag_star - (2.5 * (np.log10(1 / contrast)))
+    mag_drop = mag_obj - mag_combined
+    bottom_flux = 10 ** ((mag_combined - mag_obj) * 0.4)
+    mag_drop = mag_drop
+    bottom_flux = bottom_flux
+    return mag_drop, bottom_flux
+
+
+def read_lc_file(lc_file, usecols=None, skiprows=0):
+    """ Reads light curve data from a file.
+
+    Parameters:
+        file (str): a file with the time and flux in the first and second columns, respectively.
+            A third column with error in flux can also be given. (required)
+        usecols (int, tuple, array): Which columns to read, with the
+                first being the time, the seconds the flux and third the flux error [optional].
+        skiprows (int): Skip the first skiprows lines, including comments; default: 0 [optional].
+    Returns:
+        :rtype: (array, array, array)
+    """
+    from astropy.io import ascii
+    from astropy.time import Time
+
+    if not os.path.isfile(lc_file):
+        raise ValueError('{} not found'.format(lc_file))
+    if usecols is not None:
+        if len(usecols) == 2:
+            try:
+                time, flux = np.loadtxt(lc_file, usecols=usecols, skiprows=skiprows, unpack=True)
+                return time, flux
+            except:
+                pass
+        elif len(usecols) == 3:
+            try:
+                time, flux, dflux = np.loadtxt(lc_file, usecols=usecols, skiprows=skiprows, unpack=True)
+                return time, flux, dflux
+            except:
+                pass
+        else:
+            raise ValueError('usecols should have 2 or 3 values')
+    else:
+        try:
+            time, flux = np.loadtxt(lc_file, usecols=[0, 1], skiprows=skiprows, unpack=True)
+            return time, flux
+        except:
+            pass
+        try:
+            time, flux, dflux = np.loadtxt(lc_file, usecols=[0, 1, 2], skiprows=skiprows, unpack=True)
+            return time, flux, dflux
+        except:
+            pass
+    try:
+        lc_data = ascii.read(lc_file, data_start=skiprows)
+
+        if usecols is not None:
+            if len(usecols) == 2:
+                time_col_index, flux_col_index = usecols
+                time_col_name = lc_data.colnames[time_col_index]
+                flux_col_name = lc_data.colnames[flux_col_index]
+                time, flux = Time(lc_data[time_col_name].data).jd, lc_data[flux_col_name].data
+                return time, flux
+            elif len(usecols) == 3:
+                time_col_index, flux_col_index, dflux_col_index = usecols
+                time_col_name = lc_data.colnames[time_col_index]
+                flux_col_name = lc_data.colnames[flux_col_index]
+                dflux_col_name = lc_data.colnames[dflux_col_index]
+                time, flux = Time(lc_data[time_col_name].data).jd, lc_data[flux_col_name].data
+                dflux = lc_data[dflux_col_name].data
+                return time, flux, dflux
+            else:
+                raise ValueError('usecols should have 2 or 3 values')
+        else:
+            if len(lc_data.colnames) == 2:
+                time_col_index, flux_col_index = [0, 1]
+                time_col_name = lc_data.colnames[time_col_index]
+                flux_col_name = lc_data.colnames[flux_col_index]
+                time, flux = Time(lc_data[time_col_name].data).jd, lc_data[flux_col_name].data
+                return time, flux
+            elif len(lc_data.colnames) == 3:
+                time_col_index, flux_col_index, dflux_col_index = [0, 1, 2]
+                time_col_name = lc_data.colnames[time_col_index]
+                flux_col_name = lc_data.colnames[flux_col_index]
+                dflux_col_name = lc_data.colnames[dflux_col_index]
+                time, flux = Time(lc_data[time_col_name].data).jd, lc_data[flux_col_name].data
+                dflux = lc_data[dflux_col_name].data
+                return time, flux, dflux
+            else:
+                raise ValueError('file columns  should have 2 or 3 values')
+    except:
+        raise ValueError('An error has occurred in reading the {}'.format(lc_file))
