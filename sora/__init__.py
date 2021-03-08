a2f9f4d971ebadbebe99036672d563e551880b76
Altair Gomes <altairgomesjr@gmail.com>
2021-03-08 17:43:35 -0300

    Update initialization of the SORA package.

--- sora/__init__.py
+++ sora/__init__.py
@@ -1,9 +1,10 @@
-from .ephem import EphemKernel, EphemPlanete, EphemJPL
+from .body import Body
+from .ephem import EphemKernel, EphemPlanete, EphemJPL, EphemHorizons
+from .lightcurve import LightCurve
 from .observer import Observer
 from .star import Star
-from .lightcurve import LightCurve
 from .occultation import Occultation
-from .body import *
-
 
 __version__ = '0.2dev'
+
+print(f'SORA version: {__version__}')
